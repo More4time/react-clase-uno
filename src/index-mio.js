1d87d0835d5541c84ec9ff5fbecfd857bf91d226
More4time <lautaromoreno1998@live.com>
2022-09-26 11:24:08 -0300

    react-clase-uno
    Primer commit

---
+++ src/index-mio.js
@@ -0,0 +1,17 @@
+
+
+console.log("PROBANDO PROBANDO")
+
+
+
+import { Tarjeta } from './tarea/Tarea1.js';
+
+ReactDOM.render(
+  <Tarjeta
+    nombre="Julián Absatz"
+    titulo="Programador Front End"
+    imagen="https://avatars2.githubusercontent.com/u/14203988?s=460&v=4"
+  />,
+  document.getElementById('react-app')
+);
+
